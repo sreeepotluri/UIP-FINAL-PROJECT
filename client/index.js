b2aead1814ad95437dcef7edf9bcbb4f6d73a01f
sreeepotluri <potlurip1@newpaltz.edu>
2022-07-09 16:14:03 -0400

    Just Added Comments

--- client/index.js
+++ client/index.js
@@ -30,4 +30,4 @@ app.use('/user', userRoutes);
 app.use('/post', postRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Server is running on port 5000`));
